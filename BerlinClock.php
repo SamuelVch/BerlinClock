d165594d928e862a946d5124a02c411bca97b9b6
Ali Koksal <ali.koksal@student.vinci.be>
2020-11-05 18:06:24 +0100

    refactor - remove useless if in methods createtable5Minutes, createTable5Hours, createTableMinutes and createTable5Minutes

--- BerlinClock.php
+++ BerlinClock.php
@@ -7,11 +7,11 @@ class BerlinClock
     private $array5Minutes = ["black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black"];
     private $arrayHours = ["black", "black", "black", "black"];
     private $array5Hours = ["black", "black", "black", "black"];
-    #private $arrayAllClock = ["black","black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black"];
 
     public function translate(int $int): array
     {
-        return $this->createTableSimpleMinute($int);
+        $nbrIteration = ($int % 5);
+        return $this->createTableSimpleMinute($nbrIteration);
     }
 
     public function translate5Minutes(int $int): array
@@ -39,29 +39,26 @@ class BerlinClock
         return ["black"];
     }
 
-    public function translateAllClock(int $date){
+    public function translateAllClock(int $date)
+    {
         return $this->createTableAllClock($date);
     }
 
-    public function createTableSimpleMinute(int $int): array
+    public function createTableSimpleMinute(int $nbrIteration): array
     {
-        for ($i = 0; $i < 4; $i++) {
-            if ($i < $int) {
-                $this->arrayMinutes[$i] = "yellow";
-            }
+        for ($i = 0; $i < $nbrIteration; $i++) {
+            $this->arrayMinutes[$i] = "yellow";
         }
         return $this->arrayMinutes;
     }
 
     public function createTable5Minutes(int $nbrIteration): array
     {
-        for ($i = 0; $i < 11; $i++) {
-            if($i < $nbrIteration) {
-                if (($i + 1) % 3 === 0) {
-                    $this->array5Minutes[$i] = "red";
-                } else {
-                    $this->array5Minutes[$i] = "yellow";
-                }
+        for ($i = 0; $i < $nbrIteration; $i++) {
+            if (($i + 1) % 3 === 0) {
+                $this->array5Minutes[$i] = "red";
+            } else {
+                $this->array5Minutes[$i] = "yellow";
             }
         }
         return $this->array5Minutes;
@@ -69,17 +66,17 @@ class BerlinClock
 
     public function createTableSimpleHour(int $nbrIteration): array
     {
-        for($i = 0; $i < $nbrIteration; $i++) {
+        for($i = 0; $i < $nbrIteration; $i++)
+        {
             $this->arrayHours[$i] = "red";
         }
         return $this->arrayHours;
     }
 
-    public function createTable5Hours(int $nbrIteration): void{
-        for($i =0; $i<4;$i++){
-            if($i<$nbrIteration) {
-                $this->array5Hours[$i] = "red";
-            }
+    public function createTable5Hours(int $nbrIteration): void
+    {
+        for($i =0; $i<$nbrIteration;$i++){
+            $this->array5Hours[$i] = "red";
         }
     }
 
